4414b28769483b4dbf20dc03f69c5ba320654cce
Dilshan454 <dilshanchathuranga1634@gmail.com>
2025-05-17 10:29:27 +0000

    1st commit

--- 01.java
+++ 01.java
@@ -1,3 +1,21 @@
 class Student{
+    private int studentId;
+    private String name;
+    private int daysAttended;
 
+    Student(int studentId,String name,int daysAttended){
+        this.studentId=studentId;
+        this.name=name;
+        this.daysAttended=daysAttended;
+    }
+
+    public int getStudentId(){
+        return studentId;
+    }
+    public String getName(){
+		return name;
+    }
+     public int getDaysAttended(){
+        return daysAttended;
+    }
 }
