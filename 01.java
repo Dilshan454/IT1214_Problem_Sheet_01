3c4534afceba89b9c846f099ff4c4a9c494e1c38
Dilshan454 <dilshanchathuranga1634@gmail.com>
2025-05-18 09:45:56 +0000

    fix errors

--- 01.java
+++ 01.java
@@ -25,14 +25,18 @@ class Student{
     public void setName(String name){
         this.name=name;
     }
-    public void setAttended(int daysAttended) { 
+    public void setDaysAttended(int daysAttended) { 
         this.daysAttended = daysAttended;
+    }
+
+    public String toString(){
+        return "ID: "+studentId+", Name: "+name+", "+daysAttended;
     }
 }
 
 class Classroom{
     Student[] students=new Student[10];
-    int studentcount=0;
+    int studentCount=0;
 
     public void addStudent(Student student){
         if(studentCount<10){
@@ -48,14 +52,24 @@ class Classroom{
             }
         }
         System.out.println("Student ID "+id+" not found.");
-}
+    }
+
+    public void displayStudents() {
+        for (int i = 0; i < studentCount; i++) {
+            System.out.println(students[i]);
+        }
+    }
 }
 
-class AttendanceSystem {
+class Main {
     public static void main(String[] args) {
         Classroom classroom = new Classroom();
-        classroom.addStudent(new Student(101, "Alice Smith", 12));
+        classroom.addStudent(new Student(101, "Saman", 12));
+        classroom.addStudent(new Student(102, "Bob", 15));
+        classroom.addStudent(new Student(103, "Nimal", 10));
 
+        classroom.updateAttendance(102, 16); 
+        classroom.updateAttendance(104, 12); 
 
         classroom.displayStudents();
     }
