85ce929b8e5e22f7a0f72daf2e65e0186910772d
Dilshan454 <dilshanchathuranga1634@gmail.com>
2025-05-18 07:29:33 +0000

    first question

--- 01.java
+++ 01.java
@@ -18,4 +18,45 @@ class Student{
      public int getDaysAttended(){
         return daysAttended;
     }
+
+    public void setStudentId(int studentId){
+        this.studentId=studentId;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setAttended(int daysAttended) { 
+        this.daysAttended = daysAttended;
+    }
+}
+
+class Classroom{
+    Student[] students=new Student[10];
+    int studentcount=0;
+
+    public void addStudent(Student student){
+        if(studentCount<10){
+            students[studentCount++]=student;
+        }
+    }
+
+    public void updateAttendance(int id, int newDays) {
+        for (int i = 0; i < studentCount; i++) {
+            if (students[i].getStudentId() == id) {
+                students[i].setDaysAttended(newDays);
+                return;
+            }
+        }
+        System.out.println("Student ID "+id+" not found.");
+}
+}
+
+class AttendanceSystem {
+    public static void main(String[] args) {
+        Classroom classroom = new Classroom();
+        classroom.addStudent(new Student(101, "Alice Smith", 12));
+
+
+        classroom.displayStudents();
+    }
 }
