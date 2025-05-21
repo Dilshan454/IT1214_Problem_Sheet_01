c73b5cefb3a2fd1cf4cc756416cd0c8c42d41b0d
Dilshan454 <dilshanchathuranga1634@gmail.com>
2025-05-21 19:18:25 +0000

    3rd question finsh

--- 03/BankManager.java
+++ 03/BankManager.java
@@ -23,7 +23,7 @@ class BankAccount{
     }
 
     //setter
-    void setAccountNumber(int accoutNumber){
+    void setAccountNumber(int accountNumber){
         this.accountNumber=accountNumber;
     }
     void setAccountHolder(String accountHolder){
@@ -42,11 +42,63 @@ class BankAccount{
     }
 }
 
+class Bank{
+    private BankAccount[] accounts;
+    private int accountCount;
+
+    Bank(){
+        accounts=new BankAccount[5];
+        accountCount=0;
+    }
+
+    //Add new bank account
+    void addAccount(BankAccount account){
+        if(accountCount<accounts.length){
+            accounts[accountCount]=account;
+            accountCount++;
+        }
+        else{
+            System.out.println("Cannot add more accounts. Bank is full.");
+        }
+    }
+
+    //withdraw money using account number
+    void withdrawFromAccount(int accountNumber, double amount){
+        for(int i=0;i<accountCount;i++){
+            if (accounts[i].getAccountNumber() == accountNumber){
+                try{
+                    accounts[i].withdraw(amount);
+                    System.out.println("Withdrawn " + amount + " from account " + accountNumber);
+                }
+                catch(IllegalArgumentException e){
+                    System.out.println("Error: " + e.getMessage());
+                }
+                return;
+            }
+        }
+        System.out.println("Account number " + accountNumber + " not found.");
+    }
+
+
+    //display details
+    void displayAllAccounts(){
+        if(accountCount==0){
+            System.out.println("No accounts to display.");
+        }
+        else{
+            System.out.println("Account Number\tHolder\tBalance");
+            for (int i = 0; i < accountCount; i++){
+                System.out.println(accounts[i].getAccountNumber()+"\t\t"+accounts[i].getAccountHolder()+"\t"+accounts[i].getBalance());
+            }
+        }
+    }
+}
+
 
 public class BankManager{
     public static void main(String[] args){
         //create account
-        BankAccount acc=new BankAccount(1001, "Alice", 5000.0);
+       /* BankAccount acc=new BankAccount(1001, "Alice", 5000.0);
 
         System.out.println("Account Number: " + acc.getAccountNumber());
         System.out.println("Holder: " + acc.getAccountHolder());
@@ -55,6 +107,24 @@ public class BankManager{
 
         //withdraw
         acc.withdraw(1000);
-        System.out.println("after withdraw Balance: " + acc.getBalance());
+        System.out.println("after withdraw Balance: " + acc.getBalance());*/
+
+
+
+        Bank bank = new Bank();
+
+        bank.addAccount(new BankAccount(1001, "Alice", 5000.0));
+        bank.addAccount(new BankAccount(1002, "Bob", 3000.0));
+
+        // Withdraw unvalid amount
+        bank.withdrawFromAccount(1001, 6000.0);
+
+        // Withdraw valid amount
+        bank.withdrawFromAccount(1002, 1000.0);
+
+        // Display all accounts
+        System.out.println("\nAll Bank Accounts:");
+        bank.displayAllAccounts();
+
     }
 }
